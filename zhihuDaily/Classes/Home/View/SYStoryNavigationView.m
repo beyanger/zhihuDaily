6b3b30f77fee7d9c3c7608a3753f33811108a742
beyanger <beyanger@gmail.com>
2016-02-24 10:39:35 +0800

    添加点赞的动画效果

--- zhihuDaily/Classes/Home/View/SYStoryNavigationView.m
+++ zhihuDaily/Classes/Home/View/SYStoryNavigationView.m
@@ -19,17 +19,63 @@
 
 @implementation SYStoryNavigationView
 
-- (void)updatePopularity {
-    self.popularityLabel.textColor = self.popularityButton.selected ? SYColor(22, 164, 220, 1.) : [UIColor lightGrayColor];
-    
+- (void)awakeFromNib {
+    [self.popularityButton addObserver:self forKeyPath:@"selected" options:NSKeyValueObservingOptionNew context:nil];
 }
+
+
+- (void)dealloc {
+    [self.popularityButton removeObserver:self forKeyPath:@"selected"];
+}
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"selected"]) {
+            self.popularityLabel.textColor = self.popularityButton.selected ? SYColor(22, 164, 220, 1.) : [UIColor lightGrayColor];
+        if (self.popularityButton.selected) {
+            self.popularityLabel.text = [NSString stringWithFormat:@"%d", self.popularityLabel.text.intValue+1];
+#warning  添加动画
+            [self changeValue:self.popularityButton];
+        } else {
+            self.popularityLabel.text = [NSString stringWithFormat:@"%d", self.popularityLabel.text.intValue-1];
+        }
+    }
+}
+
+- (void)changeValue:(UIButton *)button {
+    UILabel *view = [[UILabel alloc] init];
+    view.backgroundColor = [UIColor blueColor];
+    view.textAlignment = NSTextAlignmentCenter;
+    view.textColor = [UIColor whiteColor];
+    view.layer.cornerRadius = 4;
+    view.clipsToBounds = YES;
+    view.text = [NSString stringWithFormat:@"%d", self.popularityLabel.text.intValue-1];
+    
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    [window addSubview:view];
+    
+    CGSize buttonSize = button.frame.size;
+    
+    CGRect originFrame = CGRectMake(buttonSize.width*0.5, buttonSize.height*0.5, 0, 0);
+    view.frame = [self.popularityButton convertRect:originFrame toView:window];
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        CGRect frame = CGRectMake(0, -15, buttonSize.width, 15);
+        view.frame = [button convertRect:frame toView:window];
+    } completion:^(BOOL finished) {
+        view.text = self.popularityLabel.text;
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [view removeFromSuperview];
+        });
+    }];
+}
+
 
 
 - (void)setExtraStory:(SYExtraStory *)extraStory {
     _extraStory = extraStory;
     self.popularityLabel.text = [NSString stringWithFormat:@"%ld", extraStory.popularity];
     self.commentsLabel.text = [NSString stringWithFormat:@"%ld", extraStory.comments];
-    [self updatePopularity];
 }
 
 
@@ -42,13 +88,6 @@
     }
     if (sender.tag == self.popularityButton.tag) {
         self.popularityButton.selected = !self.popularityButton.selected;
-        [self updatePopularity];
-        if (self.popularityButton.selected) {
-            self.popularityLabel.text = [NSString stringWithFormat:@"%d", self.popularityLabel.text.intValue+1];
-            // 添加动画
-        } else {
-            self.popularityLabel.text = [NSString stringWithFormat:@"%d", self.popularityLabel.text.intValue-1];
-        }
     }
 }
 
